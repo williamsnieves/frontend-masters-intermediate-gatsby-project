ccbf04433974627c9b3b0dc31ddd2f29ad1a43c6
Willians Nieves <williamsnieves@gmail.com>
2021-11-28 00:52:08 +0100

    feat: second update

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -1,3 +1,60 @@
+const fetch = require('node-fetch');
+const { createRemoteFileNode } = require('gatsby-source-filesystem');
+const authors = require('./src/data/authors.json');
+const books = require('./src/data/books.json');
+
+exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
+  const { createNode, createTypes } = actions;
+
+  createTypes(`
+    type Author implements Node {
+      books: [Book]! @link(from: "slug", by: "author.slug")
+    }
+
+    type Book implements Node {
+      author: Author! @link(from: "author", by: "slug")
+    }
+  `);
+
+  authors.forEach((author) => {
+    createNode({
+      ...author,
+      id: createNodeId(`author-${author.slug}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Author',
+        content: JSON.stringify(author),
+        contentDigest: createContentDigest(author),
+      },
+    });
+  });
+
+  books.forEach((book) => {
+    createNode({
+      ...book,
+      id: createNodeId(`book-${book.isbn}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Book',
+        content: JSON.stringify(book),
+        contentDigest: createContentDigest(book),
+      },
+    });
+  });
+  const node = {
+    id: createNodeId(`some-unique-string`),
+    parent: null,
+    cheildren: [],
+    internal: {
+      type: 'MyCustomType',
+      content: JSON.stringify(nodeContent),
+      contentDigest: createContentDigest(nodeContent),
+    },
+  };
+};
+
 exports.createPages = ({ actions }) => {
   const { createPage } = actions;
 
@@ -11,4 +68,56 @@ exports.createPages = ({ actions }) => {
       },
     },
   });
+};
+
+exports.createResolvers = ({
+  actions,
+  cache,
+  createNodeId,
+  createResolvers,
+  store,
+  reporter,
+}) => {
+  const { createNode } = actions;
+  const resolvers = {
+    Book: {
+      buyLink: {
+        type: 'String',
+        resolve: (source) =>
+          `https://www.powells.com/searchresults?keyword=${source.isbn}`,
+      },
+      cover: {
+        type: 'File',
+        resolve: async (source) => {
+          const response = await fetch(
+            `https://openlibrary.org/isbn/${source.isbn}.json`,
+          );
+
+          if (!response.ok) {
+            console.warn(
+              `Error loading details from ${source.isbn} - got ${response.status}`,
+            );
+            return null;
+          }
+
+          const { covers } = await response.json();
+
+          if (covers.length) {
+            return createRemoteFileNode({
+              url: `https://covers.openlibrary.org/b/id/${covers[0]}-L.jpg`,
+              store,
+              cache,
+              createNode,
+              createNodeId,
+              reporter,
+            });
+          } else {
+            return null;
+          }
+        },
+      },
+    },
+  };
+
+  createResolvers(resolvers);
 };
